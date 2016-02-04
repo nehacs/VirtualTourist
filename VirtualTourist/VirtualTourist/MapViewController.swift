707ecac1a5bb1f0892b43b6ea94af049bc66eca6
nehacs <U6030939@u6030939-tpm-a.ten.thomsonreuters.com>
2016-02-04 14:04:22 -0800

    User can drop a pin on map

--- VirtualTourist/VirtualTourist/MapViewController.swift
+++ VirtualTourist/VirtualTourist/MapViewController.swift
@@ -17,6 +17,43 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         self.mapView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let uilgr = UILongPressGestureRecognizer(target: self, action: "addAnnotation:")
+        uilgr.minimumPressDuration = 1.0
+        mapView.addGestureRecognizer(uilgr)
+    }
+    
+   func addAnnotation(gestureRecognizer:UIGestureRecognizer) {
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            let touchPoint = gestureRecognizer.locationInView(mapView)
+            let newCoordinates = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            
+            CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: newCoordinates.latitude, longitude: newCoordinates.longitude), completionHandler: {(placemarks, error) -> Void in
+                
+                // TODO: Might need to move this into dispatch async
+                if error != nil {
+                    print("Reverse geocoder failed with error" + error!.localizedDescription)
+                    return
+                }
+                
+                if placemarks!.count > 0 {
+                    let pm = placemarks![0] as CLPlacemark
+                    
+                    // not all places have thoroughfare & subThoroughfare so validate those values
+                    annotation.title = pm.thoroughfare! + ", " + pm.subThoroughfare!
+                    annotation.subtitle = pm.subLocality
+                    self.mapView.addAnnotation(annotation)
+                }
+                else {
+                    annotation.title = "Unknown Place"
+                    self.mapView.addAnnotation(annotation)
+                    print("Problem with the data received from geocoder")
+                }
+                self.mapView.addAnnotation(annotation)
+            })
+        }
     }
 }
 
