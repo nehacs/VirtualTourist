5dbf56989270c007e506410340cb05c1fac0a041
Neha Agarwal <neha.cs@gmail.com>
2016-04-01 12:04:47 -0700

    Small changes

--- VirtualTourist/VirtualTourist/CollectionViewController.swift
+++ VirtualTourist/VirtualTourist/CollectionViewController.swift
@@ -18,6 +18,11 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var newCollection: UIBarButtonItem!
     
+    // Keep the changes. We will keep track of insertions, deletions, and updates.
+    var insertedIndexPaths: [NSIndexPath]!
+    var deletedIndexPaths: [NSIndexPath]!
+    var updatedIndexPaths: [NSIndexPath]!
+    
     var annotation: MKAnnotation!
     var photos = [PhotoData]()
     
@@ -39,12 +44,18 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
         collectionView.dataSource = self
         
         // Step 2: Perform the fetch
+        var error: NSError?
         do {
             try fetchedResultsController.performFetch()
-        } catch {}
+        } catch let error1 as NSError {
+            error = error1
+        }
+        
+        if let error = error {
+            print("Error performing initial fetch: \(error)")
+        }
         
         // Step 6: Set the delegate to this view controller
-        fetchedResultsController.delegate = self
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -61,16 +72,17 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     
     lazy var fetchedResultsController: NSFetchedResultsController = {
         
-        let fetchRequest = NSFetchRequest(entityName: "Location")
+        let fetchRequest = NSFetchRequest(entityName: "Photo")
         
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
-        fetchRequest.predicate = NSPredicate(format: "lat == %@", self.annotation.coordinate.latitude);
-        fetchRequest.predicate = NSPredicate(format: "long == %@", self.annotation.coordinate.longitude);
+        fetchRequest.sortDescriptors = []
+//        fetchRequest.predicate = NSPredicate(format: "lat == %@", self.annotation.coordinate.latitude);
+//        fetchRequest.predicate = NSPredicate(format: "long == %@", self.annotation.coordinate.longitude);
         
         let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
             managedObjectContext: self.sharedContext,
             sectionNameKeyPath: nil,
             cacheName: nil)
+        fetchedResultsController.delegate = self
         
         return fetchedResultsController
         
@@ -125,6 +137,14 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
         fetchPhotos()
     }
 
+    func configureCell(cell: CollectionViewCell, atIndexPath indexPath: NSIndexPath) {
+        
+        let photo = self.fetchedResultsController.objectAtIndexPath(indexPath) as! PhotoData
+        
+        let photoImageView = UIImageView(image: photo.image)
+        cell.photo.image = photoImageView.image
+    }
+    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         let sectionInfo = self.fetchedResultsController.sections![section]
         
@@ -134,11 +154,12 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("photoReuseID", forIndexPath: indexPath) as! CollectionViewCell
         
-        let photo = photos[indexPath.row]
-        let photoImageView = UIImageView(image: photo.image)
-
-        photoImageView.contentMode = UIViewContentMode.Redraw
-        cell.photo.image = photoImageView.image
+//        let photo = photos[indexPath.row]
+//        let photoImageView = UIImageView(image: photo.image)
+//
+//        photoImageView.contentMode = UIViewContentMode.Redraw
+//        cell.photo.image = photoImageView.image
+        self.configureCell(cell, atIndexPath: indexPath)
         return cell
     }
     
@@ -148,52 +169,78 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     
     // MARK: - Fetched Results Controller Delegate
     
+    // Whenever changes are made to Core Data the following three methods are invoked. This first method is used to create
+    // three fresh arrays to record the index paths that will be changed.
     func controllerWillChangeContent(controller: NSFetchedResultsController) {
-//        self.collectionView.beginUpdates()
+        // We are about to handle some new changes. Start out with empty arrays for each change type
+        insertedIndexPaths = [NSIndexPath]()
+        deletedIndexPaths = [NSIndexPath]()
+        updatedIndexPaths = [NSIndexPath]()
+        
+        print("in controllerWillChangeContent")
     }
     
-    func controller(controller: NSFetchedResultsController,
-        didChangeSection sectionInfo: NSFetchedResultsSectionInfo,
-        atIndex sectionIndex: Int,
-        forChangeType type: NSFetchedResultsChangeType) {
-            
-//            switch type {
-//            case .Insert:
-//                self.tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
-//                
-//            case .Delete:
-//                self.tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
-//                
-//            default:
-//                return
-//            }
-    }
-    
+    // The second method may be called multiple times, once for each Color object that is added, deleted, or changed.
+    // We store the incex paths into the three arrays.
     func controller(controller: NSFetchedResultsController,
         didChangeObject anObject: AnyObject,
         atIndexPath indexPath: NSIndexPath?,
         forChangeType type: NSFetchedResultsChangeType,
         newIndexPath: NSIndexPath?) {
-            
-//            switch type {
-//            case .Insert:
-//                tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
-//                
-//            case .Delete:
-//                tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
-//                
-//            case .Update:
-//                let cell = tableView.cellForRowAtIndexPath(indexPath!) as! ActorTableViewCell
-//                let movie = controller.objectAtIndexPath(indexPath!) as! Movie
-//                self.configureCell(cell, movie: movie)
-//                
-//            case .Move:
-//                tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
-//                tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
-//            }
+            switch type{
+                
+            case .Insert:
+                print("Insert an item")
+                // Here we are noting that a new Color instance has been added to Core Data. We remember its index path
+                // so that we can add a cell in "controllerDidChangeContent". Note that the "newIndexPath" parameter has
+                // the index path that we want in this case
+                insertedIndexPaths.append(newIndexPath!)
+                break
+            case .Delete:
+                print("Delete an item")
+                // Here we are noting that a Color instance has been deleted from Core Data. We keep remember its index path
+                // so that we can remove the corresponding cell in "controllerDidChangeContent". The "indexPath" parameter has
+                // value that we want in this case.
+                deletedIndexPaths.append(indexPath!)
+                break
+            case .Update:
+                print("Update an item.")
+                // We don't expect Color instances to change after they are created. But Core Data would
+                // notify us of changes if any occured. This can be useful if you want to respond to changes
+                // that come about after data is downloaded. For example, when an images is downloaded from
+                // Flickr in the Virtual Tourist app
+                updatedIndexPaths.append(indexPath!)
+                break
+            case .Move:
+                print("Move an item. We don't expect to see this in this app.")
+                break
+            }
     }
     
+    // This method is invoked after all of the changed in the current batch have been collected
+    // into the three index path arrays (insert, delete, and upate). We now need to loop through the
+    // arrays and perform the changes.
+    //
+    // The most interesting thing about the method is the collection view's "performBatchUpdates" method.
+    // Notice that all of the changes are performed inside a closure that is handed to the collection view.
     func controllerDidChangeContent(controller: NSFetchedResultsController) {
-//        self.tableView.endUpdates()
+        
+        print("in controllerDidChangeContent. changes.count: \(insertedIndexPaths.count + deletedIndexPaths.count)")
+        
+        collectionView.performBatchUpdates({() -> Void in
+            
+            for indexPath in self.insertedIndexPaths {
+                self.collectionView.insertItemsAtIndexPaths([indexPath])
+            }
+            
+            for indexPath in self.deletedIndexPaths {
+                self.collectionView.deleteItemsAtIndexPaths([indexPath])
+            }
+            
+            for indexPath in self.updatedIndexPaths {
+                self.collectionView.reloadItemsAtIndexPaths([indexPath])
+            }
+            
+            }, completion: nil)
     }
 }
